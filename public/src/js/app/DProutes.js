21f912c617da31919a104a07701d4b256064ca1b
Matth96 <Matthias.Dolstra@hva.nl>
2016-05-28 16:35:20 +0200

    add ;

--- public/src/js/app/DProutes.js
+++ public/src/js/app/DProutes.js
@@ -1,18 +1,18 @@
 DP.routes = (function() {
     var pathname = function(path) {
         return window.location.pathname === path;
-    }
+    };
 
     var _checkRoutes = function() {
         if (pathname('/admin/posters/add')) {
             DP.poster.uploadPreview();
         }
-    }
+    };
     var init = function() {
         _checkRoutes();
-    }
+    };
 
     return {
         init: init
-    }
+    };
 })();
