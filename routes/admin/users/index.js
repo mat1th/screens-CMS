6b7165acf52f765c8603b0e09752fcc4c6272f77
Matth96 <Matthias.Dolstra@hva.nl>
2016-05-28 21:10:20 +0200

    check user is admin

--- routes/admin/users/index.js
+++ routes/admin/users/index.js
@@ -1,48 +1,70 @@
 var fs = require('fs'),
     express = require('express'),
     moment = require('moment'),
-    checklogin = require('../../../modules/checklogin.js'),
+    credentials = require('../../../modules/credentials.js'),
     isValidDate = require('../../../modules/isValidDate.js'),
     router = express.Router();
 
 router.get('/', function(req, res, next) {
-    if (req.session.email) {
-        req.getConnection(function(err, connection) {
-            var sql = 'SELECT email, name, role FROM users';
-            // Get the user id using username
-            connection.query(sql, function(err, match) {
-                if (err) {
-                    throw err;
-                }
-                if (match !== '' && match.length > 0) {
-                    res.render('admin/users/show', {
-                        title: 'Users',
-                        logedin: checklogin(req.session),
-                        data: match
-                    });
-                } else {
-                    res.render('admin/users/show', {
-                        title: 'Users',
-                        logedin: checklogin(req.session),
-                        error: 'You have no displays jet',
-                        data: match
-                    });
-                }
+    var cr = credentials(req.session),
+        login = cr.login,
+        email = cr.email,
+        admin = cr.admin,
+        sql;
+
+    if (login) {
+        if (admin) {
+            req.getConnection(function(err, connection) {
+                sql = 'SELECT email, name, role FROM users';
+                // Get the user id using username
+                connection.query(sql, function(err, match) {
+                    if (err) {
+                        throw err;
+                    }
+                    if (match !== '' && match.length > 0) {
+                        res.render('admin/users/show', {
+                            title: 'Users',
+                            rights: {
+                                admin: admin,
+                                logedin: login
+                            },
+                            data: match
+                        });
+                    } else {
+                        res.render('admin/users/show', {
+                            title: 'Users',
+                            rights: {
+                                admin: admin,
+                                logedin: login
+                            },
+                            error: 'You have no displays jet',
+                            data: match
+                        });
+                    }
+                });
             });
-        });
+        } else {
+            res.redirect('/admin');
+        }
     } else {
         res.redirect('/users/login');
     }
 });
 
 router.get('/add', function(req, res, next) {
-    var login = checklogin(req.session);
-    if (login) {
+    var cr = credentials(req.session),
+        login = cr.login,
+        admin = cr.admin;
+
+    if (login && admin) {
         res.render('admin/users/add', {
             title: 'Add a poster',
+            rights: {
+                admin: admin,
+                logedin: login
+            },
             postUrl: '/admin/slideshows/add',
-            error: false,
-            logedin: login
+            error: false
         });
     } else {
         res.redirect('/users/login');
