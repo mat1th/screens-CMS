bf108545b83306fabf6ccc8e8ab6d07d44efd3ea
Matth96 <Matthias.Dolstra@hva.nl>
2016-05-31 11:30:56 +0200

    add abliaty to drag posters in a order

--- public/src/js/app/DPslideshows.js
+++ public/src/js/app/DPslideshows.js
@@ -1,7 +1,9 @@
 DP.slideshows = (function() {
     var _plusButton = DP.helper.select('.add'),
         _plusButtonIcon = DP.helper.select('.add svg'),
-        _selector = DP.helper.select('.slideshow-picker');
+        _selector = DP.helper.select('.slideshow-picker'),
+        _imagerowLi = DP.helper.selectAll('.imagerow');
+
 
     var _addNewPoster = function() {
         _plusButton.addEventListener('click', function() {
@@ -9,9 +11,60 @@ DP.slideshows = (function() {
             _plusButtonIcon.classList.toggle('rotate-right');
         });
     };
+    var _sortable = function() {
+        // inspiration form http://codepen.io/agate1/pen/vOoRvj
+        var item;
+        var ul;
+        var posterrow = DP.helper.select('#posterrow');
+        var posteritems = DP.helper.selectAll('.posteritem');
+
+        posterrow.addEventListener('dragenter', function(ev) {
+            ev.preventDefault();
+            if (listNumber(ev.target) > listNumber(item)) {
+                ul.insertBefore(ev.target, item);
+            } else {
+                ul.insertBefore(item, ev.target);
+            }
+            return true;
+        });
+
+        posterrow.addEventListener('dragend', function() {
+            item.classList.remove('placeholder');
+            return false;
+        });
+        posterrow.addEventListener('drop', function functionName(ev) {
+            ev.stopPropagation();
+            return false;
+        });
+        posterrow.addEventListener('dragover', function functionName() {
+            return false;
+        });
+
+        for (var p = 0; p < posteritems.length; p++) {
+            posteritems[p].addEventListener('dragstart', function(ev) {
+                item = ev.target;
+                ul = ev.target.parentNode;
+                ev.dataTransfer.effectAllowed = 'move';
+                ev.dataTransfer.setData('Text', item.id);
+                item.ondrag = function() {
+                    item.classList.add('placeholder');
+                };
+                return true;
+            });
+        }
+
+        function listNumber(el) {
+            for (var i = 0; i < ul.children.length; i++) {
+                if (ul.children[i].getAttribute('id') === el.id) {
+                    return i;
+                }
+            }
+        }
+    };
 
     var init = function() {
         _addNewPoster();
+        _sortable();
     };
 
     return {
