cabcd439f1c071a12bd94b1ac434a1c0e7c0985a
Matth96 <Matthias.Dolstra@hva.nl>
2016-05-31 14:49:32 +0200

    add a postData function to post new data

--- public/src/js/app/DPhelper.js
+++ public/src/js/app/DPhelper.js
@@ -11,7 +11,7 @@ DP.helper = (function() {
                     var anHttpRequest = new XMLHttpRequest();
 
                     anHttpRequest.onreadystatechange = function() {
-                        if (anHttpRequest.readyState === 4 && anHttpRequest.status === 200) {
+                        if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200) {
                             aCallback(anHttpRequest.responseText);
                         }
                     };
@@ -21,11 +21,27 @@ DP.helper = (function() {
             } else {
                 return false;
             }
+        },
+        postData = function(url, params) {
+            var http = new XMLHttpRequest();
+            http.open('POST', url, true);
+
+            //Send the proper header information along with the request
+            http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+            http.onreadystatechange = function() { //Call a function when the state changes.
+
+                if (http.readyState == 4 && http.status == 200) {
+                    console.log(http.responseText);
+                }
+            };
+            http.send(params);
         };
 
     return {
         select: select,
         selectAll: selectAll,
-        getData: getData
+        getData: getData,
+        postData: postData
     };
 })();
