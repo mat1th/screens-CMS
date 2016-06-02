d8d2f21011d64eac9c861c2a1133742f428fa1bf
Matth96 <Matthias.Dolstra@hva.nl>
2016-06-02 11:21:32 +0200

    add variable max to randnumber

--- modules/randnumber.js
+++ modules/randnumber.js
@@ -1,6 +1,6 @@
-var randNumber = function() {
+var randNumber = function(max) {
     var number = Math.random();
-    return Math.floor(number  * (1000000000 - 0) + 0);
-}
+    return Math.floor(number  * (max - 0) + 0);
+};
 
 module.exports = randNumber;
