2ba74cc406e31a4bbcd6fc6fd8fd2235804ca569
Matth96 <Matthias.Dolstra@hva.nl>
2016-06-09 15:37:29 +0200

    add background color fade animation

--- public/src/js/slideshow/script.js
+++ public/src/js/slideshow/script.js
@@ -47,59 +47,91 @@ slideshow.start = (function() {
         animate = function(element, animation, duration, type, color, id) {
             if (animation === 'left-push') {
                 slider.set(element, {
-                    onComplete: slideshow.vimeo.play,
-                    onCompleteParams: [type],
-                    x: 0,
-                    opacity: 0
-                }).to(element, animationTime, {
-                    x: windowWidth,
-                    opacity: 1,
-                    ease: Power4.easeIn
-                }).to(element, animationTime, {
-                    // x: windowWidth * 2,
-                    // opacity: 0,
-                    // ease: Power4.easeIn,
-                    delay: duration,
-                    onComplete: slideshow.vimeo.pauze,
-                    onCompleteParams: [type]
-                }, '-=' + animationTime);
+                        onComplete: slideshow.vimeo.play,
+                        onCompleteParams: [type],
+                        x: 0,
+                        opacity: 0
+                    })
+                    .to(body, animationTime, {
+                        backgroundColor: color
+                    }, 'animation' + id)
+                    , 'animation' + id + '-=1'.to(element, animationTime, {
+                        x: windowWidth,
+                        opacity: 1,
+                        ease: Power4.easeIn
+                    })
+                    .to(element, animationTime, {
+                        // x: windowWidth * 2,
+                        // opacity: 0,
+                        // ease: Power4.easeIn,
+                        delay: duration,
+                        onComplete: slideshow.vimeo.pauze,
+                        onCompleteParams: [type]
+                    }, '-=' + animationTime);
             } else if (animation === 'top-push') {
-                slider.yoyo(false).set(element, {
-                    onComplete: slideshow.vimeo.play,
-                    onCompleteParams: [type],
-                    y: 0,
-                    opacity: 0
-                }).to(element, animationTime, {
-                    y: windowHeight,
-                    opacity: 1,
-                    ease: Power4.easeIn
-                }).to(element, animationTime, {
-                    // x: windowHeight * 2,
-                    // opacity: 0,
-                    // ease: Power4.easeIn,
-                    delay: duration,
-                    onComplete: slideshow.vimeo.pauze,
-                    onCompleteParams: [type]
-                }, '-=' + animationTime);
+                slider.to(body, animationTime, {
+                        backgroundColor: color
+                    }, 'animation' + id).yoyo(false).set(element, {
+                        onComplete: slideshow.vimeo.play,
+                        onCompleteParams: [type],
+                        y: 0,
+                        opacity: 0
+                    }, 'animation' + id + '-=1')
+                    .to(element, animationTime, {
+                        y: windowHeight,
+                        opacity: 1,
+                        ease: Power4.easeIn
+                    })
+                    .to(element, animationTime, {
+                        // x: windowHeight * 2,
+                        // opacity: 0,
+                        // ease: Power4.easeIn,
+                        delay: duration,
+                        onComplete: slideshow.vimeo.pauze,
+                        onCompleteParams: [type]
+                    }, '-=' + animationTime);
             } else if (animation === 'pop') {
-                slider.fromTo(element, animationTime, {
-                    onComplete: slideshow.vimeo.play,
-                    onCompleteParams: [type],
-                    opacity: 0,
-                    scale: 0,
-                    rotation: -180
-                }, {
-                    opacity: 1,
-                    scale: 1,
-                    rotation: 0
-                }).to(element, animationTime, {
-                    // opacity: 0,
-                    // scale: 0,
-                    // rotation: -180,
-                    delay: duration,
-                    onComplete: slideshow.vimeo.pauze,
-                    onCompleteParams: [type]
-                }, '-=' + animationTime);
+                slider.to(body, animationTime, {
+                        backgroundColor: color
+                    }, 'animation' + id)
+                    .fromTo(element, animationTime, {
+                        onComplete: slideshow.vimeo.play,
+                        onCompleteParams: [type],
+                        opacity: 0,
+                        scale: 0,
+                        rotation: -180
+                    }, {
+                        opacity: 1,
+                        scale: 1,
+                        rotation: 0
+                    }, 'animation' + id + '-=1')
+                    .to(element, animationTime, {
+                        // opacity: 0,
+                        // scale: 0,
+                        // rotation: -180,
+                        delay: duration,
+                        onComplete: slideshow.vimeo.pauze,
+                        onCompleteParams: [type]
+                    }, '-=' + animationTime);
+            } else if (animation === 'fadein') {
+                slider.to(body, animationTime, {
+                        backgroundColor: color
+                    }, 'animation' + id)
+                    .fromTo(element, animationTime, {
+                        onComplete: slideshow.vimeo.play,
+                        onCompleteParams: [type],
+                        opacity: 0
+                    }, {
+                        opacity: 1
+                    }, 'animation' + id + '-=1')
+                    .to(element, animationTime, {
+                        delay: duration,
+                        onComplete: slideshow.vimeo.pauze,
+                        onCompleteParams: [type]
+                    }, '-=' + animationTime);
+                // .to(element, 1, {
+                //     opacity: 0
+                // }, '+=10');
             }
         };
     return {
