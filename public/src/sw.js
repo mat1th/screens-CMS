1009933e74656703ba985e1df63fee989adb4b5b
Matth96 <Matthias.Dolstra@hva.nl>
2016-06-12 17:02:38 +0200

    fix some issues with service worker

--- public/src/sw.js
+++ public/src/sw.js
@@ -1,12 +1,13 @@
 const currentCacheName = 'DP-app-v1';
 const casheContent = [
-        './',
-        '',
-        '../css/style.css',
-        '../img/background.jpg',
-        '../lib/director.min.js',
-        '../js/app.js'
-      ];
+    '/',
+    '/admin/',
+    '/admin/screens',
+    '/admin/slideshows',
+    '/css/style.css',
+    '/img/background.jpg',
+    '/js/app.js'
+];
 
 //install service worker
 this.addEventListener('install', event => {
@@ -31,22 +32,22 @@ this.addEventListener('fetch', event => {
                             }
                             return response;
                         });
-                }).catch(htmlFallback)
+                })
         })
     )
 });
 
 this.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames
-          .filter(cacheName => (cacheName.startsWith('ll-app-')))
-          .filter(cacheName => (cacheName !== currentCacheName))
-          .map(cacheName => caches.delete(cacheName))
-      );
-    })
-  );
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames
+                .filter(cacheName => (cacheName.startsWith('ll-app-')))
+                .filter(cacheName => (cacheName !== currentCacheName))
+                .map(cacheName => caches.delete(cacheName))
+            );
+        })
+    );
 });
 
 
