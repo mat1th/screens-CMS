ebed4f1106910e4591c2ec4ef30d3ab4ce6ef9e0
Matth96 <Matthias.Dolstra@hva.nl>
2016-06-19 12:49:09 +0200

    lazyload css when cookie is not set

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,20 @@
 var fs = require('fs'),
     express = require('express'),
     credentials = require('../modules/credentials.js'),
+    renderTemplate = require('../modules/renderTemplate.js'),
     router = express.Router();
 
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
     var cr = credentials(req.session),
         login = cr.login;
-        
+
     res.render('home', {
         title: 'Home',
         pagelayout: 'transparant',
         rights: {
             logedin: login
-        }
+        },
+        styleCookie: req.cookies.style
     });
 });
 
